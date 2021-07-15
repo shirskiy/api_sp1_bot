ca7c2764c0016483250fbba52786d6269b3738c2
Дмитрий <dapklllblp@yandex.ru>
2021-07-15 16:52:55 +0300

    15/07.1

--- homework.py
+++ homework.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Настройка логов
 logging.basicConfig(
     level=logging.DEBUG,
     filename='main.log',
@@ -17,17 +16,14 @@ logging.basicConfig(
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
 )
 logger = logging.getLogger('main.log')
-handler = RotatingFileHandler('main.log', maxBytes=33000, backupCount=1)
-logger.addHandler(handler)
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+core_url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
@@ -38,46 +34,49 @@ def parse_homework_status(homework):
             verdict = 'К сожалению, в работе нашлись ошибки.'
         elif homework_status == 'reviewing':
             verdict = 'Работа взята в ревью'
-        else:
+        elif homework_status == 'approved':
             verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+        else:
+            verdict = (f'Работе присвоен статус "{homework_status}".'
+                       '\nОбратитесь к куратору, что бы уточнить ситуацию.')
         return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     except Exception as e:
-        logging.exception(e, exc_info=True)
+        logger.info(f'error {e}')
         time.sleep(1)
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(url, headers=headers, params=payload)
+        homework_statuses = requests.get(
+            core_url, headers=headers, params=payload)
     except ConnectionError as e:
-        logging.exception(e, exc_info=True)
+        logger.info(f'error {e}')
         time.sleep(1)
     return homework_statuses.json()
 
 
 def send_message(message):
-    logging.info(f'отправка сообщения {message}')
+    logger.info(f'отправка сообщения {message}')
     return bot.send_message(CHAT_ID, message)
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = 0  # УДОЛИ
+    # current_timestamp = int(time.time())  # Начальное значение timestamp
 
     while True:
         try:
             current_homework = get_homeworks(
                 current_timestamp)['homeworks'][0]
             send_message(parse_homework_status(current_homework))
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            time.sleep(5 * 60)
 
         except Exception as e:
-            logging.exception(e, exc_info=True)
+            logger.info(f'error {e}')
             send_message(f'Бот упал с ошибкой: {str(e)}')
-            print(f'Бот упал с ошибкой: {e}')
-            exit()  # Выход после ошибки, что бы не создавать спам от бота.
+            exit()
 
 
 if __name__ == '__main__':
